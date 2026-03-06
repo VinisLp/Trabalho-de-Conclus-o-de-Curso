1b332d99d39744bb48aa1c86698f4a3fb8bdcc7c
VinisLp <vinislopes@example.com>
2026-03-06 20:45:35 -0300

    Correção: Limpar arquivos temporários de migração - repositório transferido para UrbApp-tcc

--- teste_migracao.sh
+++
@@ -1,9 +0,0 @@
-# Teste da Migração para Organização UrbApp-tcc
-
-# Execute este comando APÓS transferir o repositório no GitHub:
-git push
-
-# Se funcionar, a migração foi bem-sucedida!
-# Se der erro, execute:
-git remote set-url origin git@github.com:VinisLp/Trabalho-de-Conclus-o-de-Curso.git
-# E tente novamente a transferência no GitHub
